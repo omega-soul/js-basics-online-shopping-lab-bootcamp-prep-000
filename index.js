7a1beb1499fc5dc57ff6520142640aa4615683bc
vincent <omegaortiz1997@gmail.com>
2018-05-23 11:51:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,20 +9,49 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  var Object = {
+    itemName: itemName, itemPrice: Math.floor(Math.random() * 100)
+  };
+  cart.push(Object);
+  return (`${itemName} has been added to your cart.`);
 }
 
+
+
+
+
+
+
 function viewCart() {
-  // write your code here
+  for (let i = 0; i<3; i++) {
+    if (getCart().length < 0) {
+      return "Your shopping cart is empty.";
+    }
+    else if (i>=1) {
+      return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+    } else if (i>=2) {
+      return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`;
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
 
 function total() {
   // write your code here
 }
 
 function removeFromCart(item) {
-  // write your code here
+// write your code here
 }
 
 function placeOrder(cardNumber) {
